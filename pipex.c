d8306e3bd28cc9d9ebea23dafebe7328c0fd3859
AlexHanchak <ohanchak@gmail.com>
2023-01-23 12:16:01 +0100

    e

--- pipex.c
+++ pipex.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -10,20 +11,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys.wait.h>
 
-int	pipex(void)
+int main(int argc, char *argv[])
 {
-	return (0);
-}
+    int fd[2];
+    if (pipe(fd) == -1)
+        return (1);
 
-int	main(int argc, char *argv[])
-{
-    pipe();
+    int pid1 = fork();
+    if (pid1 < 0)
+        return (2);
+//child
+    if (pid1 == 0)
+        execlp("ping", "-c", "5", "google.com", NULL);
+
+    waitpid(pid1, NULL, 0);
+
     return (0);
 }
-s
