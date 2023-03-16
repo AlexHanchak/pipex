17c2b336199d157dbffe12fbbbb8071ac1d502b3
AlexHanchak <ohanchak@gmail.com>
2023-03-16 11:47:39 +0100

    heder file in bonus change struct

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -39,7 +39,25 @@ void	close_ppipe(t_pipb *pipex)
 
 void infile(char **argv, t_pipb *pipex)
 {
-	
+	if (!ft_strncmp("here_doc", argv[1], 9))
+		here_doc(argv[2], pipex);
+	else
+	{
+		pipex->infile = open(argv[1], O_RDONLY);
+		if (pipex->infile < 0)
+			msg_error(ERR_INFILE);
+	}
+
+}
+
+void outfile(char *argv, t_pipb *pipex)
+{
+	if (pipex->here_doc)
+		pipex->outfile = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0000644);
+	else
+		pipex->outfile = open(argv, O_CREAT | O_RDWR | O_TRUNC, 0000644);
+	if (pipex->outfile < 0)
+
 }
 
 int main(int argc, char *argv[], char *envp[])
@@ -48,6 +66,7 @@ int main(int argc, char *argv[], char *envp[])
 
 	if (argc < args_in(argv[1], &pipex))
 //		return ("errror")????????????
+
 
 	return (0);
 }
